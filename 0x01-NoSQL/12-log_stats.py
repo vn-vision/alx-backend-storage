05a6a4037f385c3b73d9240d11fe24c821311c0e
vn-vision <vctrngang@gmail.com>
2024-08-13 04:41:35 -0700

    mine's better with some improvements

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,30 +1,45 @@
 #!/usr/bin/env python3
-'''Task 12's module.
 '''
+Script provides stats about Nginx logs stored in MongoDB.
+'''
+
 from pymongo import MongoClient
 
-
-def print_nginx_request_logs(nginx_collection):
-    '''Prints stats about Nginx request logs.
+def print_nginx_request_logs(nginx):
     '''
-    print('{} logs'.format(nginx_collection.count_documents({})))
-    print('Methods:')
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    for method in methods:
-        req_count = len(list(nginx_collection.find({'method': method})))
-        print('\tmethod {}: {}'.format(method, req_count))
-    status_checks_count = len(list(
-        nginx_collection.find({'method': 'GET', 'path': '/status'})
-    ))
-    print('{} status check'.format(status_checks_count))
+    Prints statistics about the Nginx logs:
+        - Total number of logs
+        - Number of logs for each HTTP method (GET, POST, PUT, PATCH, DELETE)
+        - Number of logs with method=GET and path=/status
+    '''
+    # Count total documents in nginx collection
+    print(f'{nginx.count_documents({})} logs')
 
+    # Define the methods to check
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+
+    print('Methods:')
+    for method in methods:
+        # Count documents for each method
+        count = nginx.count_documents({'method': method})
+        print(f'\tmethod {method}: {count}')
+
+    # Count documents with method=GET and path=/status
+    status_count = nginx.count_documents({'method': 'GET', 'path': '/status'})
+    print(f'{status_count} status check')
 
 def run():
-    '''Provides some stats about Nginx logs stored in MongoDB.
     '''
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    print_nginx_request_logs(client.logs.nginx)
+    Connect to the MongoDB client and run the log stats function.
+    '''
+    # Connect to the MongoDB server
+    client = MongoClient()
 
+    # Access the logs database and the nginx collection
+    nginx_collection = client.logs.nginx
+
+    # Print stats about nginx request logs
+    print_nginx_request_logs(nginx_collection)
 
 if __name__ == '__main__':
     run()
