ac3f22dab04a37fd0143e62df8fe582fc02eb6ef
vn-vision <vctrngang@gmail.com>
2024-08-13 04:32:44 -0700

    log stats - the errors

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,49 +1,45 @@
 #!/usr/bin/env python3
 '''
-script provides stats about nginx los stored in MongoDB
+Script provides stats about Nginx logs stored in MongoDB.
 '''
 
-from pymongo import MOngoClient
-
+from pymongo import MongoClient
 
 def print_nginx_request_logs(nginx):
     '''
     Database: logs
     Collection: nginx
-    Display (same as the example):
-        first line: x logs where x is the number of documents in this collection
-        second line: Methods:
-        5 lines with the number of documents with the method = ["GET", "POST", "PUT", "PATCH", "DELETE"] in this order (see example below - warning: it’s a tabulation before each line)
-        one line with the number of documents with:
+    Displays the statistics:
+        - First line: x logs where x is the number of documents in this collection.
+        - Second line: Methods:
+        - 5 lines with the number of documents with the method in ["GET", "POST", "PUT", "PATCH", "DELETE"].
+        - One line with the number of documents with:
             method=GET
             path=/status
     '''
 
-    # count documens in nginx collection
+    # Count documents in nginx collection
     print('{} logs'.format(nginx.count_documents({})))
 
     print('Methods:')
 
-    # methods in the collection
+    # Methods in the collection
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
 
-    for method in methods:  # get records for each method
-        count = len(list(nginx.find({'method':method})))
-        print('\tmethod {} : {}'.format(method, count))
+    for method in methods:  # Get records for each method
+        count = nginx.count_documents({'method': method})
+        print('\tmethod {}: {}'.format(method, count))
 
-    status_count = len(list(nginx.find({'method':'GET', 'path':'/status'})))
-
+    # Count the documents where method is GET and path is /status
+    status_count = nginx.count_documents({'method': 'GET', 'path': '/status'})
     print('{} status check'.format(status_count))
-
 
 def run():
     '''
-    run the mongo client with passed collection
+    Run the MongoDB client with the passed collection.
     '''
-
     client = MongoClient()
     print_nginx_request_logs(client.logs.nginx)
 
-
-if __name__ = '__main__':
+if __name__ == '__main__':
     run()
