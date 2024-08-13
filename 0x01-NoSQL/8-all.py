3eea51b12008b63d2058f537a43030103fc3c295
vn-vision <vctrngang@gmail.com>
2024-08-13 03:16:24 -0700

    pymongo

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,23 @@
+#!/us'''r/bin/env python3
+'''
+python function that lists all documents in a collection
+'''
+from pymongo import MongoClient
+
+def list_all(mongo_collection):
+    '''
+    returns an empty list if no documents in collection
+
+    client = MongoClient()   # establish connection to default host and port
+
+    # select the database to use
+    db = client.my_db
+
+    # select collection
+    my_collection = db.mongo_collection
+
+    if  my_collection is None:
+        return []
+    '''
+    # return list(mongo_collection.find())
+    return [doc for doc in mongo_collection.find()]
